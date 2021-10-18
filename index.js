e6ae5af0569cb1ad4ceab6e421370832690d106a
Vicente Benavides <vicente.benavides0@gmail.com>
2021-10-18 01:10:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server en Express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Esta es una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Port: ' + port);
+});
